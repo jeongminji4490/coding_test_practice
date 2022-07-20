f707a4a5ad1be64a87573a089c004564e21b7f11
MINJI <62979330+jeongminji4490@users.noreply.github.com>
2022-07-20 15:25:17 +0900

    Create boj_11654.kt

---
+++ boj_11654.kt
@@ -0,0 +1,10 @@
+import java.util.*
+import kotlin.math.ceil
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
+
+fun main() {
+    val s = Scanner(System.`in`)
+    val value = s.next().single()
+    println(value.code)
+}

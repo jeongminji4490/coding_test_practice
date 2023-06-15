d61795fc2b731c5814e97a328a35660d710f8bfb
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2023-06-15 21:32:13 +0900

    Create prog_control_number_2_0.kt

---
+++ prog_control_number_2_0.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(numLog: IntArray): String {
+        return (1..numLog.lastIndex).map {
+            when (numLog[it] - numLog[it-1]) {
+                1 -> 'w'
+                -1 -> 's'
+                10 -> 'd'
+                else -> 'a'
+            }
+        }.joinToString("")
+    }
+}
